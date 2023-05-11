a8e9d47d22a795ed51ba8acee3bc1213d4f9b4e1
殊怀丶 <lvzhihe_123@qq.com>
2023-05-11 16:04:07 +0800

    init: 初始化项目

---
+++ src/main/java/com/site/blog/my/core/service/impl/ConfigServiceImpl.java
@@ -0,0 +1,91 @@
+package com.site.blog.my.core.service.impl;
+
+import com.site.blog.my.core.dao.BlogConfigMapper;
+import com.site.blog.my.core.entity.BlogConfig;
+import com.site.blog.my.core.service.ConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class ConfigServiceImpl implements ConfigService {
+    @Autowired
+    private BlogConfigMapper configMapper;
+
+    public static final String websiteName = "殊怀丶博客";
+    public static final String websiteDescription = "";
+    public static final String websiteLogo = "/admin/dist/img/logo.png";
+    public static final String websiteIcon = "/admin/dist/img/logo.png";
+
+    public static final String yourAvatar = "/admin/dist/img/logo.png";
+    public static final String yourEmail = "lvzhihe_123@qq.com";
+    public static final String yourName = "殊怀丶";
+
+    public static final String footerAbout = "殊怀丶博客";
+    public static final String footerICP = "";
+    public static final String footerCopyRight = "殊怀丶";
+    public static final String footerPoweredBy = "http://lvshuhuai.cn/";
+    public static final String footerPoweredByURL = "http://lvshuhuai.cn/";
+
+    @Override
+    public int updateConfig(String configName, String configValue) {
+        BlogConfig blogConfig = configMapper.selectByPrimaryKey(configName);
+        if (blogConfig != null) {
+            blogConfig.setConfigValue(configValue);
+            blogConfig.setUpdateTime(new Date());
+            return configMapper.updateByPrimaryKeySelective(blogConfig);
+        }
+        return 0;
+    }
+
+    @Override
+    public Map<String, String> getAllConfigs() {
+        //获取所有的map并封装为map
+        List<BlogConfig> blogConfigs = configMapper.selectAll();
+        Map<String, String> configMap = blogConfigs.stream().collect(Collectors.toMap(BlogConfig::getConfigName, BlogConfig::getConfigValue));
+        for (Map.Entry<String, String> config : configMap.entrySet()) {
+            if ("websiteName".equals(config.getKey()) && !StringUtils.hasText(config.getValue())) {
+                config.setValue(websiteName);
+            }
+            if ("websiteDescription".equals(config.getKey()) && !StringUtils.hasText(config.getValue())) {
+                config.setValue(websiteDescription);
+            }
+            if ("websiteLogo".equals(config.getKey()) && !StringUtils.hasText(config.getValue())) {
+                config.setValue(websiteLogo);
+            }
+            if ("websiteIcon".equals(config.getKey()) && !StringUtils.hasText(config.getValue())) {
+                config.setValue(websiteIcon);
+            }
+            if ("yourAvatar".equals(config.getKey()) && !StringUtils.hasText(config.getValue())) {
+                config.setValue(yourAvatar);
+            }
+            if ("yourEmail".equals(config.getKey()) && !StringUtils.hasText(config.getValue())) {
+                config.setValue(yourEmail);
+            }
+            if ("yourName".equals(config.getKey()) && !StringUtils.hasText(config.getValue())) {
+                config.setValue(yourName);
+            }
+            if ("footerAbout".equals(config.getKey()) && !StringUtils.hasText(config.getValue())) {
+                config.setValue(footerAbout);
+            }
+            if ("footerICP".equals(config.getKey()) && !StringUtils.hasText(config.getValue())) {
+                config.setValue(footerICP);
+            }
+            if ("footerCopyRight".equals(config.getKey()) && !StringUtils.hasText(config.getValue())) {
+                config.setValue(footerCopyRight);
+            }
+            if ("footerPoweredBy".equals(config.getKey()) && !StringUtils.hasText(config.getValue())) {
+                config.setValue(footerPoweredBy);
+            }
+            if ("footerPoweredByURL".equals(config.getKey()) && !StringUtils.hasText(config.getValue())) {
+                config.setValue(footerPoweredByURL);
+            }
+        }
+        return configMap;
+    }
+}
